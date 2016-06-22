332c07c86d820801e55fa89c519dbd23bcf160eb
Oota Kazuki <oota.kazuki@jp.fujitsu.com>
2016-06-22 19:20:44 +0900

    色々追加

---
+++ WpfDataGridSampleApp/WpfDataGridSampleApp/ViewModels/FrozenColumnCountViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfDataGridSampleApp.Models;
+
+namespace WpfDataGridSampleApp.ViewModels
+{
+    public class FrozenColumnCountViewModel : ViewModelBase
+    {
+        public FrozenColumnCountViewModel(Main model) : base(model)
+        {
+        }
+    }
+}
---
+++ WpfDataGridSampleApp/WpfDataGridSampleApp/ViewModels/StylingViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfDataGridSampleApp.Models;
+
+namespace WpfDataGridSampleApp.ViewModels
+{
+    public class StylingViewModel : ViewModelBase
+    {
+        public ReadOnlyReactiveCollection<EnumLabelPair<Gender>> Genders { get; }
+
+        public StylingViewModel(Main model) : base(model)
+        {
+            this.Genders = this.Model
+                .Genders
+                .ToReadOnlyReactiveCollection()
+                .AddTo(Disposable);
+        }
+    }
+}
